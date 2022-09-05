f7232edabd33d301cf6f50cb47068b15d0c7f48f
Abdullahi <aweabdullahi@gmail.com>
2022-09-05 20:04:46 +0100

    Completed the UI of the app

--- Calculator/assets/main.js
+++ Calculator/assets/main.js
@@ -8,13 +8,24 @@ function layout() {
                     '4', '5', '6',   
                     '1', '2', '3', 
                     '0', '.', 'AC'];
+    
+    const grid_ares = ['plus', 'minus', 'times', 'divide',
+                      'seven', 'eight', 'nine', 'equal',
+                      'four', 'five', 'six', 
+                      'one', 'two', 'three',
+                      'zero', 'dot', 'AC']
 
 
-    for (const i of order) {
-
+    for (var i = 0; i < order.length; i++) {
+        
         let b = document.createElement('button')
-        b.textContent = i
+
+        let extra = (i < 4) ? "background-color:rgb(211, 221, 229);" : ""
+        b.textContent = order[i]
         b.onclick = callback
+        b.setAttribute('style', `grid-area:${grid_ares[i]};${extra}`)
+        b.id = grid_ares[i] + '_btn';
+
 
         document.getElementById('button-container').appendChild(b)
     }
