be65f7adb345e2f2a51baa1cf87564a45f59ca33
Abdullahi <aweabdullahi@gmail.com>
2022-12-29 14:56:46 +0100

    Finished logic of TicTacToe

--- TicTacToe/assets/main.js
+++ TicTacToe/assets/main.js
@@ -1,19 +1,20 @@
-var tempBoxes, curr, gameOver;
+var tempBoxes, curr, gameOver, board, states, turn, stopGame;
 
-const board = [[0, 0, 0],
-               [0, 0, 0],
-               [0, 0, 0]
-]
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
 
-const states = [[0, 0, 0],
-               [0, 0, 0],
-               [0, 0, 0]
-]
+states = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
 
-var turn = "X"
+stopGame = false;
+turn = "X"
+
 const turnHeading = document.getElementById("turn-heading")
 const boxes = document.getElementsByClassName("box")
 
+const boxesEqual = (g, h, f) => g === h && g === f && g !== ""
 
 function assignStates() {
     tempBoxes = [...boxes]
@@ -22,25 +23,86 @@ function assignStates() {
         for (c = 0; c < 3; c++) {
             curr = tempBoxes.shift()
             states[r][c] = curr.innerHTML
+            board[r][c] = curr
         }
     }
+
 }
 
 function checkWin() {
-    // for (i = 0; i < 3; i++) {
-    //     if (states[i][0]==states[i][1]==states[i][2]!=0) {
-            
-    //     }
-    // }
+    for (i = 0; i < 3; i++) {
+        if (boxesEqual(states[i][0], states[i][1], states[i][2]))  {
+            board[i][0].classList.add("win-box")
+            board[i][1].classList.add("win-box")
+            board[i][2].classList.add("win-box")
+            stopGame = true
+        }
+    }
+    for (i = 0; i < 3; i++) {
+        if (boxesEqual(states[0][i], states[1][i], states[2][i]))  {
+            board[0][i].classList.add("win-box")
+            board[1][i].classList.add("win-box")
+            board[2][i].classList.add("win-box")
+            stopGame = true
+        }
+    }
 
-    gameOver = Array(...boxes).every((value) => value.innerHTML == 'X' || value.innerHTML == "O")
+    if (boxesEqual(states[0][0], states[1][1], states[2][2]))  {
+            board[0][0].classList.add("win-box")
+            board[1][1].classList.add("win-box")
+            board[2][2].classList.add("win-box")
+            stopGame = true
+    }
+    if (boxesEqual(states[2][0], states[1][1], states[0][2]))  {
+            board[2][0].classList.add("win-box")
+            board[1][1].classList.add("win-box")
+            board[0][2].classList.add("win-box")
+            stopGame = true
+    }
 
-    if (gameOver) turnHeading.innerHTML = "GAME OVER"
+    if (stopGame) {
+        box = document.querySelector(".win-box")
+
+        turnHeading.innerHTML = `Player ${box.innerHTML} wins!!`
+        
+        return restart()
+    }
+
+    boxesFull = Array(...boxes).every((value) => value.innerHTML == 'X' || value.innerHTML == "O")
+        
+    if (boxesFull) {
+        turnHeading.innerHTML = "GAME OVER! DRAW!!"
+        stopGame = true
+
+        for (box of boxes) box.classList.add("win-box")
+        restart()
+    } 
+
+   
 }
 
+function restart() {
+    setTimeout(() => {
+        for (box of boxes) {
+            box.classList.remove("win-box", "playerx", "playero")
+            box.innerHTML = ""
+        }
+        board = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+
+        states = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+        
+        turn = "X"
+        turnHeading.innerHTML = "Player X turn"
+        stopGame = false; 
+    }, 3000);
+}
 
 function callback() {
-    if (this.innerHTML) return
+    if (this.innerHTML || stopGame) return
 
     if (turn == "X") {
         this.innerHTML = "X"
